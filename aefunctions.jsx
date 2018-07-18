e33f88d21dd5e23593c7509a01700c3de592a366
Tim Haywood <tim@haywood.org>
2018-07-18 11:45:41 +1000

    Added commaNum

--- aefunctions.jsx
+++ aefunctions.jsx
@@ -186,7 +186,7 @@
         }
     
         return count;
-    }
+    },
 
     "padNumber": function(num, length) {
 
@@ -199,6 +199,33 @@
         }
 
         return (numString)
+    },
+
+    "commaNum": function(number) {
+        
+        // Expression courtesy of Dab Ebberts
+        number = '' + Math.round(number);
+        
+        if (number.length > 3) {
+
+            var mod = number.length % 3;
+            var output = (mod > 0 ? (number.substring(0, mod)) : '');
+        
+            for (i = 0; i < Math.floor(number.length / 3); i++) {
+                
+                if ((mod == 0) && (i == 0)) {
+                    output += number.substring(mod + 3 * i, mod + 3 * i + 3);
+                } else {
+                    output += ',' + number.substring(mod + 3 * i, mod + 3 * i + 3);
+                }
+            }
+
+            return (output);
+
+        } else {
+
+            return number;
+        }
     }
 
 }
