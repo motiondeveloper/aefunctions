422cfcafc2f2b91fb24a4976ecef28747f0fd02f
timhaywood <tim@haywood.org>
2019-11-04 15:24:43 +1100

    change to object return model

--- aefunctions.jsx
+++ aefunctions.jsx
@@ -1,8 +1,8 @@
-{
-    "attachKeys": function(time, inKeys = 2, outKeys = 2) {
+{ "getFunctions": function() {
 
+    function attachKeys(inKeys = 2, outKeys = 2, time = thisLayer.time) }
         if (inKeys >= 1 && outKeys >= 1) { // There is in and out animation
-    
+            
             const outStart = thisLayer.outPoint - (key(numKeys).time - key(numKeys - outKeys).time);
             const inFinish = thisLayer.inPoint + (key(inKeys).time - key(1).time);
     
@@ -24,7 +24,7 @@
             } else {
                 return valueAtTime(key(1).time + time - outStart);
             }
-
+    
         } else if (inKeys >= 2 && outKeys == 0) { // Animation in only
     
             const inFinish = thisLayer.inPoint + (key(inKeys).time - key(1).time);
@@ -39,10 +39,9 @@
         } else { // Default option if no range specified
             return value;
         }
-    },
-
-    "bounceKeys": function(time, amp = .12, freq = 2.5, decay = 8, keyMin = 1, keyMax = numKeys) {
-
+    }
+    
+    function bounceKeys(amp = .12, freq = 2.5, decay = 8, keyMin = 1, keyMax = thisProperty.numKeys, time = thisLayer.time) {
         let curKey = 0;
         let t = 0;
         
@@ -65,30 +64,28 @@
         } else {
             return value;
         }
-    },
-	    
-    "pointsToPath": function(points, closed = true) {
-
-        const pathPoints = points.map(item => fromCompToSurface(item));
-	    return createPath(pathPoints, [], [], closed);
-    },
-	    
-    "gridPoints": function(rows, columns, rowNum, columnNum, gridSize = [thisComp.width, thisComp.height]) {
+    }
     
-	    const columnWidth = gridSize[0] / columns;
-	    const rowHeight = gridSize[1] / rows;
-
-	    const topLeft = [columnWidth * (columnNum - 1), rowHeight * (rowNum -1)];
-	    const topRight = topLeft + [columnWidth, 0];
-
-	    const bottomLeft = topLeft + [0, rowHeight];
-	    const bottomRight = topRight + [0, rowHeight];
-
-	    return [topLeft, topRight, bottomRight, bottomLeft];
-    },
-
-    "hideLayerWhenBelow": function(layerIndex = index - 1) {
-
+    function getPathFromPoints(points, closed = true) {
+        const pathPoints = points.map(item => fromCompToSurface(item));
+        return createPath(pathPoints, [], [], closed);
+    }
+    
+    function gridPoints(rows, columns, rowNum, columnNum, gridSize = [thisComp.width, thisComp.height]) {
+    
+        const columnWidth = gridSize[0] / columns;
+        const rowHeight = gridSize[1] / rows;
+    
+        const topLeft = [columnWidth * (columnNum - 1), rowHeight * (rowNum -1)];
+        const topRight = topLeft + [columnWidth, 0];
+    
+        const bottomLeft = topLeft + [0, rowHeight];
+        const bottomRight = topRight + [0, rowHeight];
+    
+        return [topLeft, topRight, bottomRight, bottomLeft];
+    }
+    
+    function hideLayerWhenBelow(layerIndex = index - 1) {
         let aboveLayer;
         try {
             aboveLayer = thisComp.layer(layerIndex);
@@ -103,21 +100,19 @@
             // Layer is first layer
             return 100;
         }
-    },
-
-    "isometricPosition": function(pointControl, offset) {
-	
-         const xGrid = effect(pointControl)("Point")[0];
-         const yGrid = effect(pointControl)("Point")[1];
+    }
     
-         const x = (xGrid*1.75 - yGrid) ;
-         const y = (xGrid + yGrid/1.75)
+    function getIsometricPosition(pointControl, offset) {
+        const xGrid = effect(pointControl)("Point")[0];
+        const yGrid = effect(pointControl)("Point")[1];
     
-        return offset + [x,y]
-    },
-
-    "layerBoundsPath": function(buffer = 0, sourceLayer = thisLayer, extend = false, sampleTime = time) {
-
+        const x = (xGrid*1.75 - yGrid) ;
+        const y = (xGrid + yGrid/1.75)
+    
+       return offset + [x,y]
+    }
+    
+    function getLayerBoundsPath(buffer = 0, sourceLayer = thisLayer, extend = false, sampleTime = time) {
         const layerWidth = sourceLayer.sourceRectAtTime(sampleTime, extend).width;
         const layerHeight = sourceLayer.sourceRectAtTime(sampleTime, extend).height;
         const layerTop = sourceLayer.sourceRectAtTime(sampleTime, extend).top;
@@ -131,23 +126,20 @@
         ];
     
         return createPath(points = maskPoints, inTangents = [], outTangents = [], is_closed = true);
-    },
-
-    "layerSize": function(layerIndex, sampleTime = time) {
-        
-        var layerSize = [
+    }
+    
+    function layerSize(layerIndex, sampleTime = time) { 
+        const layerSize = [
             thisComp.layer(layerIndex).sourceRectAtTime(sampleTime, false).width,
             thisComp.layer(layerIndex).sourceRectAtTime(sampleTime, false).height
             
         ];
         return(layerSize);
-    },
-
-    "effectSearch": function(effectName) {
-
+    }
+    
+    function effectSearch(effectName) {
         const totalEffects = thisLayer("Effects").numProperties;
         let selectEffects = 0;
-    
         if (effectName != null) {
     
             for (i = 1; i <= totalEffects; i++) {
@@ -160,163 +152,88 @@
         } else {
             return totalEffects;
         }
-    },
-
-    "textCount": function(sourceText, type = word) {
+    }
     
+    function textCount(sourceText, type = "word") {
         switch (type) {
-    
             case "word":
                 return sourceText.split(" ").length;
             case "line":
-                return sourceText.split(/[^\r\n]+/g).length;
+                return sourceText.split(/[^\r\n\3]+/g).length;
             case "char":
                 return sourceText.length;
             default:
                 return null;
         }
-    },
-
-    "padNumber": function(num, length) {
-
-        return `${num}`.padStart(length, '0');
-    },
-
-    "commaNum": function(number) {
-        
+    }
+    
+    function padNumber(number, length) {
+        return `${number}`.padStart(length, '0');
+    }
+    
+    function commaNum(number) {
         // Expression courtesy of Dab Ebberts
-        let number = '' + Math.round(number);
-        
+        let number = '' + Math.round(number);    
         if (number.length > 3) {
-
             const mod = number.length % 3;
             let output = (mod > 0 ? (number.substring(0, mod)) : '');
-        
             for (i = 0; i < Math.floor(number.length / 3); i++) {
-                
                 if ((mod == 0) && (i == 0)) {
                     output += number.substring(mod + 3 * i, mod + 3 * i + 3);
                 } else {
                     output += ',' + number.substring(mod + 3 * i, mod + 3 * i + 3);
                 }
             }
-
             return output;
         } else {
             return number;
         }
-    },
+    }
     
-    "cleanLines": function(string, maxLines, maxCharacters) {
-        const lines = string.match(/[\r\n]+/g);
+    function cleanLines(string, maxLines, maxCharacters) {
+        const lines = string.match(/[\r\n\3]+/g);
         const limitedLines = lines.map((item) => {
             return item.replace(/^\s+|\s+$/g, '').substring(0, maxCharacters);
         });
     
         return limitedLines.slice(0, maxLines + 1).join("\r");
-    },
-
-    "hideDescenders": function(string, hideTime = -500, property = thisProperty) {
-        const numLines = string.split(/[^\r\n]+/g).length;
-        const descenderFreeLines = 'X\r'.repeat(numLines - 2) + 'X'
+    }
+    
+    function hideDescenders(string, hideTime = -500, property = thisProperty) {
+        const numLines = textCount(string, 'line');
+        const descenderFreeLines = 'X\r'.repeat(numLines - 1) + 'X'
         return (property.time < hideTime) ? descenderFreeLines : string;
-    },
+    }
     
-    "keyframesToArray": function() {
-    
+    function keyframesToArray(property = thisProperty) {
         let keys = [];
-    
-        for (let i=1; i <= numKeys; i++) {
-            
+        for (let i=1; i <= property.numKeys; i++) {
             const thisKey = {
-                time: key(i).time,
-                value: key(i).value
+                time: property.key(i).time,
+                value: property.key(i).value
             };
-            
             keys.push(thisKey);
         }
-    
         return keys;
-    },
-
-    "circularMotion": function(radius, revolutionTime, startAngle = -90) {
-
+    }
+    
+    function circularMotion(radius, revolutionTime, startAngle = -90) {
         const startRadians = degreesToRadians(startAngle);
         const angularSpeed = 2 * Math.PI / revolutionTime;
         const xt = radius * Math.cos(angularSpeed * time + startRadians);
         const yt = radius * Math.sin(angularSpeed * time + startRadians);
-        
         return [xt, yt]
-    },
-
-    "circularPosition": function(radius, angle) {
-
-        // Algorithm courtesy of Xinlai Ni
-        const startAngle = degreesToRadians(angle - 90);
-        const xt = radius * Math.cos(startAngle);
-        const yt = radius * Math.sin(startAngle);
-        
-        return [xt, yt, 0]
-    },
-
-    "countdown": function(time, length, speed = 1) {
-
+    }
+    
+    function countdown(length, speed = 1, time = time) {
         const clockTime = Math.max(length - speed*(time - inPoint),0);
         const clock = Math.floor(clockTime);
         const min = Math.floor((clock%3600)/60);
         const sec = Math.floor(clock%60);
         return `${min}:${sec.padStart(2, '0')}`
-    },
-
-    "heightIsZero": function(time, layer) {
-        return layer.sourceRectAtTime(time, false) > 0;
-    },
+    }
     
-    "textLayerIsEmpty": function(layer) {
-        return layer.text.sourceText.value.length === 0;
-    },
-
-    "layerIsHidden": function(layer) {
-        return layer.transform.opacity === 0;
-    },
-
-    "layerTopLeft": function(layer, sourceTime = time) {
-        const layerRect = layer.sourceRectAtTime(sourceTime, false);
-        const layerTopCorner = [layerRect.left, layerRect.top];
-        return layer.toComp(layerTopCorner);
-    },
-
-    "layerNamesToLayers": function(layerNames) {
-        
-        return layerNames.map((layerName) => {
-            return thisComp.layer(layerName);
-        });
-    },
-
-    "layersToLayerNames": function(layers) {
-        
-        return layers.map((layer) => {
-            return layer.name;
-        });
-    },
-
-    "getNonEmptyTextLayers": function(layers) {
-        function textLayerEmpty(layer) {
-            return layer.text.sourceText.value.length === 0;
-        }
-
-        return nonEmptyLayers = layers.filter(layer => !textLayerEmpty(layer));
-    },
-
-    "textLayersAreAllEmpty": function(layers) {
-        function textLayerEmpty(layer) {
-            return layer.text.sourceText.value.length === 0;
-        }
-
-        return layers.filter(layer => textLayerEmpty(layer)).length === 0;
-    },
-
-    "scaleToFit": function(
+    function scaleToFit(
         inputSize, maxSize, toggles = {
             onlyScaleDown: false, onlyScaleUp: false,
         }
@@ -332,36 +249,60 @@
         if (toggles.onlyScaleUp) { scaleFactor = Math.max(scaleFactor, 1); }
     
         return [100 * scaleFactor, 100 * scaleFactor];
-    },
+    }
     
-    "insertLineBreaks": function(string, maxCharacters, minWords, options = {
+    function getStringWithLineBreaks(string, maxCharacters) {
+        const splitRegex = new RegExp('(.{' + maxCharacters + '}[^ ]* )', 'g');
+        return string.replace(splitRegex, '$1\n');
+    }
+    
+    function hasAShortLine(string, minWords) {
+        const lines = string.split('\n');
+        if (lines.length == 1) { return false }
+        for (let index = 0; index < lines.length; index++) {
+            const line = lines[index];
+            const words = line.split(' ');
+            if (words.length <= minWords) {
+                return true;
+            }        
+        }
+        return false;
+    }
+    
+    function insertLineBreaks(string, maxCharacters, minWords, options = {
         minCharacters = 12,
         characterStep = 4,
     }) {
-        return smartBreak(string, maxCharacters, minWords, options);
-    
         function smartBreak(string, maxCharacters, minWords) {
             const brokenString = getStringWithLineBreaks(string, maxCharacters);
             if (!hasAShortLine(brokenString, minWords) || maxCharacters < options.minCharacters) { return brokenString }
             return smartBreak(string, maxCharacters - options.characterStep, minWords);
         }
-        
-        function getStringWithLineBreaks(string, maxCharacters) {
-            const splitRegex = new RegExp('(.{' + maxCharacters + '}[^ ]* )', 'g');
-            return string.replace(splitRegex, '$1\n');
-        }
-        
-        function hasAShortLine(string, minWords) {
-            const lines = string.split('\n');
-            if (lines.length == 1) { return false }
-            for (let index = 0; index < lines.length; index++) {
-                const line = lines[index];
-                const words = line.split(' ');
-                if (words.length <= minWords) {
-                    return true;
-                }        
-            }
-            return false;
-        }
+        return smartBreak(string, maxCharacters, minWords, options);
     }
-}
+    
+    return {
+        attachKeys,
+        bounceKeys,
+        getPathFromPoints,
+        gridPoints,
+        hideLayerWhenBelow,
+        getIsometricPosition,
+        getLayerBoundsPath,
+        layerSize,
+        effectSearch,
+        effectSearch,
+        textCount,
+        padNum,
+        commaNum,
+        cleanLines,
+        hideDescenders,
+        keyframesToArray,
+        circularMotion,
+        countdown,
+        scaleToFit,
+        insertLineBreaks,
+    }
+    
+    }
+    }
