918bafd3d5ed6953f86eecc67fbaf10964015e1a
timhaywood <tim@haywood.org>
2021-01-29 11:29:31 +1100

    convert functions to class

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,15 @@ const thisProperty = new PathProperty<PathValue>([[0, 0]]);
 const thisLayer = new Layer();
 const thisComp = new Comp();
 
-function getFunctions(time: number = thisLayer.time) {
-  function attachKeys(inKeys: number = 2, outKeys: number = 2) {
+interface textBreakOption {
+  minCharacters: number;
+  characterStep: number;
+}
+
+type Anchor = 'topLeft' | 'topRight' | 'bottomRight' | 'bottomLeft';
+
+class functions {
+  attachKeys(inKeys: number = 2, outKeys: number = 2) {
     if (inKeys >= 1 && outKeys >= 1) {
       // There is in and out animation
       const outStart =
@@ -25,18 +32,18 @@ function getFunctions(time: number = thisLayer.time) {
         thisLayer.inPoint +
         (thisProperty.key(inKeys).time - thisProperty.key(1).time);
 
-      if (time < thisLayer.inPoint) {
+      if (thisLayer.time < thisLayer.inPoint) {
         return thisProperty.key(1).value;
-      } else if (time < inFinish) {
+      } else if (thisLayer.time < inFinish) {
         return thisProperty.valueAtTime(
-          thisProperty.key(1).time + (time - thisLayer.inPoint)
+          thisProperty.key(1).time + (thisLayer.time - thisLayer.inPoint)
         );
-      } else if (time < outStart) {
+      } else if (thisLayer.time < outStart) {
         return thisProperty.key(inKeys).value;
       } else {
         return thisProperty.valueAtTime(
           thisProperty.key(thisProperty.numKeys - outKeys).time +
-            time -
+            thisLayer.time -
             outStart
         );
       }
@@ -46,11 +53,11 @@ function getFunctions(time: number = thisLayer.time) {
         thisLayer.outPoint -
         (thisProperty.key(outKeys).time - thisProperty.key(1).time);
 
-      if (time < outStart) {
+      if (thisLayer.time < outStart) {
         return thisProperty.key(1).value;
       } else {
         return thisProperty.valueAtTime(
-          thisProperty.key(1).time + time - outStart
+          thisProperty.key(1).time + thisLayer.time - outStart
         );
       }
     } else if (inKeys >= 2 && outKeys == 0) {
@@ -59,11 +66,11 @@ function getFunctions(time: number = thisLayer.time) {
         thisLayer.inPoint +
         (thisProperty.key(inKeys).time - thisProperty.key(1).time);
 
-      if (time < thisLayer.inPoint) {
+      if (thisLayer.time < thisLayer.inPoint) {
         return thisProperty.key(1).value;
-      } else if (time < inFinish) {
+      } else if (thisLayer.time < inFinish) {
         return thisProperty.valueAtTime(
-          thisProperty.key(1).time + (time - thisLayer.inPoint)
+          thisProperty.key(1).time + (thisLayer.time - thisLayer.inPoint)
         );
       } else {
         return thisProperty.key(inKeys).value;
@@ -74,7 +81,7 @@ function getFunctions(time: number = thisLayer.time) {
     }
   }
 
-  function bounceKeys(
+  bounceKeys(
     amp = 0.12,
     freq = 2.5,
     decay = 8,
@@ -86,15 +93,15 @@ function getFunctions(time: number = thisLayer.time) {
 
     // Set curKey to the previous keyframe
     if (thisProperty.numKeys > 0) {
-      curKey = thisProperty.nearestKey(time).index;
-      if (thisProperty.key(curKey).time > time) {
+      curKey = thisProperty.nearestKey(thisLayer.time).index;
+      if (thisProperty.key(curKey).time > thisLayer.time) {
         curKey--;
       }
     }
 
     // Set t to the time to curKey
     if (curKey !== 0) {
-      t = time - thisProperty.key(curKey).time;
+      t = thisLayer.time - thisProperty.key(curKey).time;
     }
 
     if (curKey > 0 && curKey >= keyMin && curKey <= keyMax && t < 3) {
@@ -113,14 +120,14 @@ function getFunctions(time: number = thisLayer.time) {
     }
   }
 
-  function getPathFromPoints(points: Points, closed = true) {
+  getPathFromPoints(points: Points, closed = true) {
     const pathPoints: Points = points.map(
       item => thisLayer.fromCompToSurface(item) as Vector2D
     );
     return thisProperty.createPath(pathPoints, [], [], closed);
   }
 
-  function gridPoints({
+  gridPoints({
     rows = 3,
     columns = 3,
     rowNum = 1,
@@ -142,17 +149,17 @@ function getFunctions(time: number = thisLayer.time) {
     return [topLeft, topRight, bottomRight, bottomLeft];
   }
 
-  function hideLayerWhenBelow(layerIndex = thisLayer.index - 1) {
+  hideLayerWhenBelow(layerIndex = thisLayer.index - 1) {
     try {
       const aboveLayer = thisComp.layer(layerIndex);
-      return time < aboveLayer.inPoint ? 100 : 0;
+      return thisLayer.time < aboveLayer.inPoint ? 100 : 0;
     } catch (err) {
       // Layer is first layer
       return 100;
     }
   }
 
-  function getIsometricPosition(position: Vector2D, offset: Vector = [0, 0]) {
+  getIsometricPosition(position: Vector2D, offset: Vector = [0, 0]) {
     const xGrid = position[0];
     const yGrid = position[1];
 
@@ -162,11 +169,11 @@ function getFunctions(time: number = thisLayer.time) {
     return thisLayer.add(offset, [x, y]);
   }
 
-  function getLayerBoundsPath(
+  getLayerBoundsPath(
     buffer = 0,
     sourceLayer = thisLayer,
     extend = false,
-    sampleTime = time
+    sampleTime = thisLayer.time
   ) {
     const layerWidth = sourceLayer.sourceRectAtTime(sampleTime, extend).width;
     const layerHeight = sourceLayer.sourceRectAtTime(sampleTime, extend).height;
@@ -183,7 +190,7 @@ function getFunctions(time: number = thisLayer.time) {
     return thisProperty.createPath(maskPoints, [], [], true);
   }
 
-  function layerSize(layerIndex = thisLayer.index, sampleTime = time) {
+  layerSize(layerIndex = thisLayer.index, sampleTime = thisLayer.time) {
     const layerSize = [
       thisComp.layer(layerIndex).sourceRectAtTime(sampleTime, false).width,
       thisComp.layer(layerIndex).sourceRectAtTime(sampleTime, false).height,
@@ -191,9 +198,9 @@ function getFunctions(time: number = thisLayer.time) {
     return layerSize;
   }
 
-  function layerRect({
+  layerRect({
     layer = thisLayer,
-    sampleTime = time,
+    sampleTime = thisLayer.time,
     anchor = 'center',
     capHeight = false,
     capHeightTime = -550,
@@ -241,7 +248,7 @@ function getFunctions(time: number = thisLayer.time) {
     };
   }
 
-  function textCount(sourceText: string, type = 'word') {
+  textCount(sourceText: string, type = 'word') {
     switch (type) {
       case 'word':
         return sourceText.split(' ').length;
@@ -254,11 +261,11 @@ function getFunctions(time: number = thisLayer.time) {
     }
   }
 
-  function padNumber(number: number, length: number) {
+  padNumber(number: number, length: number) {
     return `${'0'.repeat(length)}${number}`;
   }
 
-  function commaNum(inputNum: number) {
+  commaNum(inputNum: number) {
     // Expression courtesy of Dab Ebberts
     let number = '' + Math.round(inputNum);
     if (number.length > 3) {
@@ -277,7 +284,7 @@ function getFunctions(time: number = thisLayer.time) {
     }
   }
 
-  function cleanLines(string: string, maxLines: number, maxCharacters: number) {
+  cleanLines(string: string, maxLines: number, maxCharacters: number) {
     const lines = string.split(/[\r\n\3]+/g);
     const limitedLines = lines.map(item => {
       return item.replace(/^\s+|\s+$/g, '').substring(0, maxCharacters);
@@ -286,16 +293,16 @@ function getFunctions(time: number = thisLayer.time) {
     return limitedLines.slice(0, maxLines + 1).join('\n');
   }
 
-  function hideDescenders(
+  hideDescenders(
     string: string = thisProperty.value as string,
     hideTime = -500
   ) {
-    const numLines = textCount(string, 'line');
+    const numLines = this.textCount(string, 'line');
     const descenderFreeLines = 'X\r'.repeat(numLines - 1) + 'X';
-    return time < hideTime ? descenderFreeLines : string;
+    return thisLayer.time < hideTime ? descenderFreeLines : string;
   }
 
-  function getKeyframesAsArray() {
+  getKeyframesAsArray() {
     let keys = [];
     for (let i = 1; i <= thisProperty.numKeys; i++) {
       const thisKey = {
@@ -307,15 +314,15 @@ function getFunctions(time: number = thisLayer.time) {
     return keys;
   }
 
-  function circularMotion(radius = 200, revolutionTime = 1, startAngle = -90) {
+  circularMotion(radius = 200, revolutionTime = 1, startAngle = -90) {
     const startRadians = thisLayer.degreesToRadians(startAngle);
     const angularSpeed = (2 * Math.PI) / revolutionTime;
-    const xt = radius * Math.cos(angularSpeed * time + startRadians);
-    const yt = radius * Math.sin(angularSpeed * time + startRadians);
+    const xt = radius * Math.cos(angularSpeed * thisLayer.time + startRadians);
+    const yt = radius * Math.sin(angularSpeed * thisLayer.time + startRadians);
     return [xt, yt];
   }
 
-  function circularPosition(radius: number, angle: number) {
+  circularPosition(radius: number, angle: number) {
     // Algorithm courtesy of Xinlai Ni
     const startAngle = thisLayer.degreesToRadians(angle - 90);
     const xt = radius * Math.cos(startAngle);
@@ -323,18 +330,18 @@ function getFunctions(time: number = thisLayer.time) {
     return [xt, yt];
   }
 
-  function countdown(
-    length = thisLayer.outPoint - thisLayer.inPoint,
-    speed = 1
-  ) {
-    const clockTime = Math.max(length - speed * (time - thisLayer.inPoint), 0);
+  countdown(length = thisLayer.outPoint - thisLayer.inPoint, speed = 1) {
+    const clockTime = Math.max(
+      length - speed * (thisLayer.time - thisLayer.inPoint),
+      0
+    );
     const clock = Math.floor(clockTime);
     const min = Math.floor((clock % 3600) / 60);
     const sec = Math.floor(clock % 60);
-    return `${min}:${padNumber(sec, 2)}`;
+    return `${min}:${this.padNumber(sec, 2)}`;
   }
 
-  function scaleToFit(
+  scaleToFit(
     inputSize: Vector2D,
     maxSize: Vector2D,
     toggles = {
@@ -367,12 +374,12 @@ function getFunctions(time: number = thisLayer.time) {
       : [100 * scaleFactorWidth, 100 * scaleFactorHeight];
   }
 
-  function getStringWithLineBreaks(string: string, maxCharacters: number) {
+  getStringWithLineBreaks(string: string, maxCharacters: number) {
     const splitRegex = new RegExp('(.{' + maxCharacters + '}[^ ]* )', 'g');
     return string.replace(splitRegex, '$1\n');
   }
 
-  function hasAShortLine(string: string, minWords: number) {
+  hasAShortLine(string: string, minWords: number) {
     const lines = string.split('\n');
     if (lines.length == 1) {
       return false;
@@ -387,12 +394,28 @@ function getFunctions(time: number = thisLayer.time) {
     return false;
   }
 
-  interface textBreakOption {
-    minCharacters: number;
-    characterStep: number;
+  smartBreak(
+    string: string,
+    maxCharacters: number,
+    minWords: number,
+    options: textBreakOption
+  ): string {
+    const brokenString = this.getStringWithLineBreaks(string, maxCharacters);
+    if (
+      !this.hasAShortLine(brokenString, minWords) ||
+      maxCharacters < options.minCharacters
+    ) {
+      return brokenString;
+    }
+    return this.smartBreak(
+      string,
+      maxCharacters - options.characterStep,
+      minWords,
+      options
+    );
   }
 
-  function breakWithoutOrphans(
+  breakWithoutOrphans(
     string: string,
     maxCharacters: number,
     minWords: number,
@@ -401,32 +424,10 @@ function getFunctions(time: number = thisLayer.time) {
       characterStep: 4,
     }
   ) {
-    function smartBreak(
-      string: string,
-      maxCharacters: number,
-      minWords: number,
-      options: textBreakOption
-    ): string {
-      const brokenString = getStringWithLineBreaks(string, maxCharacters);
-      if (
-        !hasAShortLine(brokenString, minWords) ||
-        maxCharacters < options.minCharacters
-      ) {
-        return brokenString;
-      }
-      return smartBreak(
-        string,
-        maxCharacters - options.characterStep,
-        minWords,
-        options
-      );
-    }
-    return smartBreak(string, maxCharacters, minWords, options);
+    return this.smartBreak(string, maxCharacters, minWords, options);
   }
 
-  function maintainScale(
-    parentLayer: Layer = thisLayer.parent as Layer
-  ): Vector {
+  maintainScale(parentLayer: Layer = thisLayer.parent as Layer): Vector {
     // Not every layer has transform properties, so
     // optional chaining is used
     return thisLayer.transform?.scale.value.map((scale, index) =>
@@ -438,8 +439,7 @@ function getFunctions(time: number = thisLayer.time) {
     ) as Vector;
   }
 
-  type Anchor = 'topLeft' | 'topRight' | 'bottomRight' | 'bottomLeft';
-  function offsetFromAnchor(
+  offsetFromAnchor(
     position: Vector,
     [offsetX, offsetY]: Vector,
     anchor: Anchor
@@ -447,48 +447,18 @@ function getFunctions(time: number = thisLayer.time) {
     switch (anchor) {
       case 'topLeft':
         return thisLayer.add(position, [-offsetX, -offsetY]);
-        break;
       case 'topRight':
         return thisLayer.add(position, [offsetX, -offsetY]);
-        break;
       case 'bottomRight':
         return thisLayer.add(position, [offsetX, offsetY]);
-        break;
       case 'bottomLeft':
         return thisLayer.add(position, [-offsetX, offsetY]);
-        break;
       default:
         throw Error('Invalid anchor: ' + anchor);
-        break;
     }
   }
-
-  return {
-    attachKeys,
-    bounceKeys,
-    getPathFromPoints,
-    gridPoints,
-    hideLayerWhenBelow,
-    getIsometricPosition,
-    getLayerBoundsPath,
-    layerSize,
-    layerRect,
-    textCount,
-    padNumber,
-    commaNum,
-    cleanLines,
-    hideDescenders,
-    getKeyframesAsArray,
-    circularMotion,
-    circularPosition,
-    countdown,
-    scaleToFit,
-    breakWithoutOrphans,
-    maintainScale,
-    offsetFromAnchor,
-  };
 }
 
 const version: string = '_npmVersion';
 
-export { getFunctions, version };
+export { functions, version };
